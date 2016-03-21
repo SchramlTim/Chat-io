4b588b9e9340823dbc4c604bcccc9124fae63920
SchramlTim <schramltim@gmail.com>
2016-03-21 18:59:30 +0100

    Funktionalität Connect Disconnect

--- chat.js
+++ chat.js
@@ -10,43 +10,100 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     var parsedMessage = JSON.parse(msg);
-    var found = false;
-    for(var i = 0; i < userList.length; i++) {
-      if(userList[i].nickname == parsedMessage.nickname){
-        found = true;
-      }
-    }
-    if(!found){
-      userList.push({"nickname":parsedMessage.nickname,"id":socket.id});
-    }
-    if(parsedMessage.message.startsWith("/w")){
+    var sender = getUsername(socket.id);
+    var currentdate = new Date();
+    var timestamp = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 
+    if(parsedMessage.message.startsWith("/w")){
       var reciever = parsedMessage.message.substr(parsedMessage.message.indexOf('"')+1,parsedMessage.message.indexOf('"',parsedMessage.message.indexOf('"')+1) - parsedMessage.message.indexOf('"')-1);
       console.log("whisper to " +reciever);
-      socketID = "";
-      for(var i = 0; i < userList.length; i++){
-        if(reciever.toLowerCase() == userList[i].nickname.toLowerCase()){
-          socketID = userList[i].id;
-        }
-      }
-      var fullMessage = "<span class='user-nickname private'>" + parsedMessage.nickname + ":</span>" + parsedMessage.message.substr(parsedMessage.message.indexOf('" ')+2,parsedMessage.message.length);
+      var socketID = getSocketID(reciever);
+      var fullMessage = "<span class='timestamp'>"+timestamp+"</span> <span class='user-nickname private'>" + sender + " (privat):</span>" + parsedMessage.message.substr(parsedMessage.message.indexOf('" ')+2,parsedMessage.message.length);
       io.to(socketID).emit('chat message', fullMessage);
       io.to(socket.id).emit('chat message', fullMessage);
-    }else{
-      var fullMessage = "<span class='user-nickname'>" + parsedMessage.nickname + ":</span>" + parsedMessage.message;
+    }
+    else if(parsedMessage.message.startsWith("/list")){
+      var tmpList = [];
+      for(var i = 0; i < userList.length; i++){
+        tmpList.push(userList[i].nickname);
+      }
+      io.emit('user list', tmpList);
+    }
+    else{
+      var fullMessage = "<span class='timestamp'>"+timestamp+"</span> <span class='user-nickname'>" + sender + ":</span>" + parsedMessage.message;
       io.emit('chat message', fullMessage);
     }
-
-
-
-    var tmpList = [];
-    for(var i = 0; i < userList.length; i++){
-      tmpList.push(userList[i].nickname);
+  });
+  socket.on('login', function(username){
+    if(NutzerEintragen(username,socket.id) && username != ""){
+      io.emit('chat message', "<span class='user-connected'>Benutzer " + username + " ist dem Chat beigetreten!</span>");
+    }else{
+      io.to(socket.id).emit('nickname error','Nickname ist schon vergeben!')
     }
-    io.emit('user list', tmpList);
+  });
+  socket.on('disconnect', function(){
+    var username = getUsername(socket.id);
+    NutzerAustragen(username,socket.id);
+    io.emit('chat message', "<span class='user-connected'>Benutzer " + username + " hat kein Bock mehr auf euch!</span>");
   });
 });
 
-http.listen(process.env.PORT, function(){
+http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function NutzerEintragen(name,socketID){
+  var eingetragen = false;
+  if(!istNutzerSchonEingetragen(name)){
+    userList.push({"nickname":name,"id":socketID});
+    eingetragen = true;
+  }
+  return eingetragen;
+}
+
+function NutzerAustragen(name,socketID){
+  var ausgetragen = false;
+  if(istNutzerSchonEingetragen(name)){
+    for(var i = 0; i < userList.length; i++) {
+      if(userList[i].nickname == name && userList[i].id == socketID){
+        userList.splice(i,1);
+        ausgetragen = true;
+        break;
+      }
+    }
+  }
+  return ausgetragen;
+}
+
+function istNutzerSchonEingetragen(name){
+  var found = false;
+  for(var i = 0; i < userList.length; i++) {
+    if(userList[i].nickname == name){
+      found = true;
+      break;
+    }
+  }
+  return found;
+}
+
+function getUsername(socketID){
+  var usernameFound = "";
+  for(var i = 0; i < userList.length; i++) {
+    if(userList[i].id == socketID){
+      usernameFound = userList[i].nickname;
+      break;
+    }
+  }
+  return usernameFound;
+}
+
+function getSocketID(username){
+  var idFound = "";
+  for(var i = 0; i < userList.length; i++) {
+    if(userList[i].nickname == username){
+      idFound = userList[i].id;
+      break;
+    }
+  }
+  return idFound;
+}
