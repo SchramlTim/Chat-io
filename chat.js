b001b394157fd1bc1417696a50e6e9d1c3227a3b
SchramlTim <schramltim@gmail.com>
2016-04-06 20:52:47 +0200

    Kommentare added
    Kommentare wurde in den Quellcode eingefügt

--- chat.js
+++ chat.js
@@ -3,10 +3,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var userList = [];
 
+/*
+* returns the chat.html to the user
+* */
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/chat.html');
 });
 
+/*
+* server listen to the event "connection".
+* if the user send the event "chat message", the server sends the message to all user.
+* if the whisper command (/w "USERNAME" MESSAGE) its the message, the server search the correct socket id of the receiver and send message to him.
+* if message starts with "/list" the server returns a list of all users (online)
+* on all messages the server adds the current timestamp
+* if the user send the event "login" the server checks the username is already taken. If taken the server sends a error message back
+* the event "disconnect" triggered if the user closed the html page or reload it
+* */
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     var parsedMessage = JSON.parse(msg);
@@ -18,7 +30,7 @@ io.on('connection', function(socket){
       var reciever = parsedMessage.message.substr(parsedMessage.message.indexOf('"')+1,parsedMessage.message.indexOf('"',parsedMessage.message.indexOf('"')+1) - parsedMessage.message.indexOf('"')-1);
       console.log("whisper to " +reciever);
       var socketID = getSocketID(reciever);
-      var fullMessage = "<span class='timestamp'>"+timestamp+"</span> <span class='user-nickname private'>" + sender + " (privat):</span>" + parsedMessage.message.substr(parsedMessage.message.indexOf('" ')+2,parsedMessage.message.length);
+      var fullMessage = "<span class='timestamp'>"+timestamp+"</span> <span class='user-nickname private'>" + "<a onclick='whisperTo(this)'>"+sender + "</a>" + " (privat):</span>" + parsedMessage.message.substr(parsedMessage.message.indexOf('" ')+2,parsedMessage.message.length);
       io.to(socketID).emit('chat message', fullMessage);
       io.to(socket.id).emit('chat message', fullMessage);
     }
@@ -48,10 +60,16 @@ io.on('connection', function(socket){
   });
 });
 
+/*
+* the http server listen on the port 3000
+* */
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+/*
+* adds the username with socket id to the userlist if the user name is not already taken
+* */
 function NutzerEintragen(name,socketID){
   var eingetragen = false;
   if(!istNutzerSchonEingetragen(name)){
@@ -61,6 +79,9 @@ function NutzerEintragen(name,socketID){
   return eingetragen;
 }
 
+/*
+* deletes the user from the userlist
+* */
 function NutzerAustragen(name,socketID){
   var ausgetragen = false;
   if(istNutzerSchonEingetragen(name)){
@@ -75,6 +96,9 @@ function NutzerAustragen(name,socketID){
   return ausgetragen;
 }
 
+/*
+* checks if the user is already in the userlist
+* */
 function istNutzerSchonEingetragen(name){
   var found = false;
   for(var i = 0; i < userList.length; i++) {
@@ -86,6 +110,9 @@ function istNutzerSchonEingetragen(name){
   return found;
 }
 
+/*
+* get the username that belongs to the socket id (parameter)
+* */
 function getUsername(socketID){
   var usernameFound = "";
   for(var i = 0; i < userList.length; i++) {
@@ -96,7 +123,9 @@ function getUsername(socketID){
   }
   return usernameFound;
 }
-
+/*
+ * get the socket id that belongs to the username (parameter)
+ * */
 function getSocketID(username){
   var idFound = "";
   for(var i = 0; i < userList.length; i++) {
